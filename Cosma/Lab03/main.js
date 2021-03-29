b6804147e06034f397c298d2fcdc90df9fae975a
Cosma Alex Vergari s284922 <s284922@studenti.polito.it>
2021-03-29 19:34:37 +0200

    Cosma: [LAB03] Added filtering capabilities

--- Cosma/Lab03/main.js
+++ Cosma/Lab03/main.js
@@ -1,19 +1,106 @@
 document.addEventListener("DOMContentLoaded", ready);
 
+const filters = {
+  all: new Filter("all", "All", "filtersAllItem", null, null),
+  important: new Filter(
+    "important",
+    "Important",
+    "filtersImportantItem",
+    null,
+    null
+  ),
+  next7days: new Filter(
+    "next7days",
+    "Next 7 Days",
+    "filtersNext7DaysItem",
+    null,
+    null
+  ),
+  today: new Filter("today", "Today", "filtersTodayItem", null, null),
+  private: new Filter("private", "Private", "filtersPrivateItem", null, null),
+};
+let filterTitleMain;
+
+function Filter(id, printableName, HTMLidentifier, HTMLElement, taskList) {
+  this.id = id;
+  this.printableName = printableName;
+  this.HTMLidentifier = HTMLidentifier;
+  this.HTMLElement = HTMLElement;
+  this.taskList = taskList;
+}
+
 function ready() {
-  const tl = new TaskList();
+  getHTMLElements();
+  registerCallbacks();
+  writeTextOnPage();
 
   const t1 = new Task(1, "phone call", true, false, dayjs());
   const t2 = new Task(2, "labs", false, false, dayjs("2021-01-01"));
   const tu = new Task(0, "ciccio", true, true);
   const t3 = new Task(3, "prova", false, false, dayjs("2021-01-02"));
 
-  tl.addTask(t1);
-  tl.addTask(t2);
-  tl.addTask(tu);
-  tl.addTask(t3);
+  filters.all.taskList = new TaskList();
+  filters.all.taskList.addTask(t1);
+  filters.all.taskList.addTask(t2);
+  filters.all.taskList.addTask(tu);
+  filters.all.taskList.addTask(t3);
 
-  tl.addToHTML();
+  updateArraysAndStats();
+  replaceTaskList(filters.all);
+}
+
+function getHTMLElements() {
+  filterTitleMain = document.getElementById("filterTitleMain");
+  activityListElement = document.getElementById("activities-list-container");
+  for (filter in filters) {
+    filters[filter].HTMLElement = document.getElementById(
+      filters[filter].HTMLidentifier
+    );
+  }
+}
+
+function registerCallbacks() {
+  for (filter in filters) {
+    filters[filter].HTMLElement.addEventListener("click", clickOnFilter);
+  }
+}
+
+function writeTextOnPage() {
+  filterTitleMain.innerText = filters.all.printableName;
+  for (filter in filters) {
+    filters[filter].HTMLElement.childNodes[0].nodeValue =
+      filters[filter].printableName;
+  }
+}
+
+function clickOnFilter(element, event) {
+  for (filter in filters) {
+    filters[filter].HTMLElement.classList.remove("active");
+  }
+  for (filter in filters) {
+    if (filters[filter].HTMLidentifier === element.srcElement.id) {
+      filterTitleMain.innerText = filters[filter].printableName;
+      replaceTaskList(filters[filter]);
+      break;
+    }
+  }
+  element.srcElement.classList.add("active");
+}
+
+function replaceTaskList(filter) {
+  activityListElement.innerHTML = "";
+
+  filter.taskList.addToHTML();
+}
+
+function updateArraysAndStats() {
+  for (filter in filters) {
+    filters[filter].taskList = filters.all.taskList.getCategory(
+      filters[filter].id
+    );
+    const badge = filters[filter].HTMLElement.querySelector("span");
+    badge.innerText = filters[filter].taskList.getLength();
+  }
 }
 
 function Task(
@@ -46,22 +133,36 @@ function Task(
     return str;
   };
 
+  this.isUrgent = () => {
+    return this.urgent;
+  };
+
+  this.isPrivate = () => {
+    return this.private_;
+  };
+
   this.getDeadline = () => deadline;
 
-  this.getDeadlineDiffString = () => {
+  this.getDeadlineDiff = () => {
     if (this.deadline == undefined) {
-      return "";
+      return undefined;
     }
     let day_diff;
-    let day_diff_string;
     day_diff = this.deadline.diff(dayjs(), "day");
+    return day_diff;
+  };
+
+  this.getDeadlineDiffString = () => {
+    const day_diff = this.getDeadlineDiff();
+    if (day_diff === undefined) {
+      return "";
+    }
     if (day_diff === 0) {
       day_diff_string = "Today";
     } else if (day_diff > 0) {
       day_diff_string = "in " + day_diff.toString() + " days";
     } else {
-        console.log(day_diff);
-      day_diff_string = "expired since " + (-1*day_diff).toString() + " days";
+      day_diff_string = "expired since " + (-1 * day_diff).toString() + " days";
     }
     return day_diff_string;
   };
@@ -79,7 +180,7 @@ function Task(
     form_check.classList.add("form-check");
     form_check.appendChild(checkbox);
     form_check.appendChild(descript);
-    const shared_icon = document.createElement('i');
+    const shared_icon = document.createElement("i");
     shared_icon.classList.add("bi-people", "p-0", "m-0", "list-item-icon");
     const exp_date = document.createElement("small");
     exp_date.classList.add("mt-1");
@@ -135,5 +236,41 @@ function TaskList() {
     this.tasks.forEach((task) => {
       activity_list.appendChild(task.HTMLoutput());
     });
+  };
+
+  this.getCategory = (category) => {
+    if (category === "all") {
+      return this;
+    } else if (category === "important") {
+      const new_list = new TaskList();
+      this.tasks
+        .filter((task) => task.isUrgent())
+        .forEach((task) => new_list.addTask(task));
+      return new_list;
+    } else if (category === "today") {
+      const new_list = new TaskList();
+      this.tasks
+        .filter((task) => task.getDeadlineDiffString() === "Today")
+        .forEach((task) => new_list.addTask(task));
+      return new_list;
+    } else if (category === "next7days") {
+      const new_list = new TaskList();
+      this.tasks
+        .filter(
+          (task) => task.getDeadlineDiff() > 0 && task.getDeadlineDiff() <= 7
+        )
+        .forEach((task) => new_list.addTask(task));
+      return new_list;
+    } else if (category === "private") {
+      const new_list = new TaskList();
+      this.tasks
+        .filter((task) => task.isPrivate())
+        .forEach((task) => new_list.addTask(task));
+      return new_list;
+    }
+  };
+
+  this.getLength = () => {
+    return this.tasks.length;
   };
 }
